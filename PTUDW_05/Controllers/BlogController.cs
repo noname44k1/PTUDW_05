35d8667dc016144282580a9cbc20fb0a3dad16d0
noname44k1 <thanh.cntt.dhv@gmail.com>
2023-10-17 08:43:42 +0700

    17/10/2023 - blog, blog detail

--- PTUDW_05/Controllers/BlogController.cs
+++ PTUDW_05/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PTUDW_05.Models;
 
 namespace PTUDW_05.Controllers
@@ -13,7 +14,33 @@ namespace PTUDW_05.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var blogs = _context.TbBlogs.Where(i => (bool)i.IsActive).OrderByDescending(i => i.BlogId).ToList();
+            if (blogs == null)
+                return NotFound();
+            ViewBag.blogComment = _context.TbBlogComments.Where(m => m.IsActive).ToList();
+            return View(blogs);
         }
+
+        [Route("/blog/{alias}-{id}.html")]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var blog = _context.TbBlogs.Where(i => i.BlogId == id && (bool)i.IsActive).FirstOrDefault();
+            if (blog == null)
+                return NotFound();
+            ViewBag.blogComment = _context.TbBlogComments.Where(i => i.BlogId == id && i.IsActive).ToList();
+            return View(blog);
+        }
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //        return NotFound();
+        //    var blog = _context.TbBlogs.Where(i => i.BlogId == id && (bool)i.IsActive).FirstOrDefaultAsync(); 
+        //    if(blog == null)
+        //        return NotFound();
+        //    ViewBag.blogComment = _context.TbBlogComments.Where(i => i.BlogId == id && i.IsActive).ToList();
+        //    return View(await blog);
+        //}
     }
 }
