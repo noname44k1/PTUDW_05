37ac22e247d8c5a45a7068196df696a37458d276
noname44k1 <thanh.cntt.dhv@gmail.com>
2023-10-24 08:37:59 +0700

    24/10/2023

---
+++ PTUDW_05/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTUDW_05.Models;
+
+namespace PTUDW_05.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly HarmicContext _context;
+
+        public ContactController(HarmicContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public bool Create(string name, string phone, string email, string message)
+        {
+            try
+            {
+                TbContact contact = new TbContact();
+                contact.Name = name;
+                contact.Phone = phone;
+                contact.Email = email;
+                contact.Message = message;
+                contact.IsRead = 0;
+                contact.CreatedDate = DateTime.Now;
+                _context.AddAsync(contact);
+                _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+}
